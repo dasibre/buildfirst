1f1dfa0bacd2512dff85ceaa0e5192b0185883ca
James Naadjie <james.naadjie@gmail.com>
2016-01-05 20:50:36 -0500

    Add concat task plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,13 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     jshint: ['Gruntfile.js'],
+    concat: {
+      js: {
+        files: {
+          'build/js/bundle.js': 'public/js/**/*.js'
+        }
+      }
+    },
     less: {
       compile: {
         files: {
@@ -11,5 +18,6 @@ module.exports = function(grunt) {
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.registerTask('default', ['jshint']);
 };
