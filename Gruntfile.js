42ed98d0ac6d711a7680c7f76db9625d0caf7490
James Naadjie <james.naadjie@gmail.com>
2016-01-05 20:57:44 -0500

    add Minify uglify tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,18 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     jshint: ['Gruntfile.js'],
+    uglify: {
+      cobra: {
+        files: {
+          'build/js/cobra.min.js': 'public/js/cobra.js'
+        }
+      },
+      bundle: {
+        files: {
+          'build/js/bundle.min.js': 'build/js/bundle.js'
+        }
+      }
+    },
     concat: {
       js: {
         files: {
@@ -19,5 +31,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('default', ['jshint']);
 };
