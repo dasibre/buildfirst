96c1bc4a8ce0b8ffe0b13ee86575a58e26af3650
James Naadjie <james.naadjie@gmail.com>
2016-01-05 20:48:17 -0500

    add globbing to less task target compile files option

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     less: {
       compile: {
         files: {
-          'build/css/compiled.css': 'public/css/layout.less'
+          'build/css/compiled.css': ['public/css/layout.less', 'public/css/**/*.less']
         }
       }
     }
